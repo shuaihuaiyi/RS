d2edfaedff549598efd6ecd36a23120d8b450bee
S-H-Y-GitHub <932495000@qq.com>
2017-03-31 19:13:28 +0800

    完成图形统计输出

--- test.m
+++ test.m
@@ -10,3 +10,59 @@ for i=1:11
     newimg = antiRsLsb(img,j);
     [data(6,i),data(7,i),data(8,i),data(9,i)]=rs(newimg);
 end
+% 创建 figure
+figure1 = figure;
+
+% 创建 axes
+axes1 = axes('Parent',figure1,...
+    'Position',[0.13 0.11 0.263229166666667 0.815]);
+hold(axes1,'on');
+
+% 使用 plot 的矩阵输入创建多行
+plot1 = plot(data(1,:),data(2:5,:),'Parent',axes1);
+set(plot1(1),'DisplayName','R+');
+set(plot1(2),'DisplayName','S+');
+set(plot1(3),'DisplayName','R-');
+set(plot1(4),'DisplayName','S-');
+
+% 创建 xlabel
+xlabel({'隐写率'});
+
+% 创建 title
+title({'LSB隐写方式'});
+
+% 创建 ylabel
+ylabel({'R,S比率'});
+
+box(axes1,'on');
+% 创建 legend
+legend1 = legend(axes1,'show');
+set(legend1,...
+    'Position',[0.349001735862759 0.699247595483372 0.0371093752483527 0.147998299671803]);
+
+% 创建 axes
+axes2 = axes('Parent',figure1,...
+    'Position',[0.584427083333334 0.105741056218058 0.263229166666665 0.815]);
+hold(axes2,'on');
+
+% 使用 plot 的矩阵输入创建多行
+plot2 = plot(data(1,:),data(6:9,:));
+set(plot2(1),'DisplayName','R+');
+set(plot2(2),'DisplayName','S+');
+set(plot2(3),'DisplayName','R-');
+set(plot2(4),'DisplayName','S-');
+
+% 创建 xlabel
+xlabel({'隐写率'});
+
+% 创建 title
+title({'抗RS分析的隐写方法'});
+
+% 创建 ylabel
+ylabel({'R,S比率'});
+
+box(axes2,'on');
+% 创建 legend
+legend2 = legend(axes2,'show');
+set(legend2,...
+    'Position',[0.802126735862759 0.608957987306201 0.0371093752483526 0.147998299671803]);
