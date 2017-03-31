e1bfa2c015fb2de4c415545bef0299b560ac5f0b
S-H-Y-GitHub <932495000@qq.com>
2017-03-31 16:08:44 +0800

    add lsb

---
+++ lsb.m
@@ -0,0 +1,12 @@
+function [result] = lsb(source,rate)
+    [m,n] = size(source);
+    col = im2col(source, [m,n], 'distinct');
+    for i = 1:m*n*rate
+        if(mod(col(i), 2)==0 && ~(mod(col(i), 2)==mod(i,2)))
+            col(i) = col(i)+1;
+        elseif(mod(col(i), 2)==1 && ~(mod(col(i), 2)==mod(i,2)))
+            col(i) = col(i)-1;
+        end
+    end
+    result = col2img(col,[m,n],'distinct');
+end
